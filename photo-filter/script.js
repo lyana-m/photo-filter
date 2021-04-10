be683bea7f5afa5aabc345ca11c6462bc9971b98
lyana <lyana-m@yandex.ru>
2021-04-10 13:33:08 +0500

    feat: implement css-filter application

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,15 +1,25 @@
-// add slider values
+/********* implement css-filters *********/
 
 const inputs = document.querySelectorAll('.filters input');
 
-function changeInputValue(event) {
- const input = event.target;
- const output = input.nextElementSibling;
- output.value = input.value;
-}
+function applyFilter(event) {
+  // add filter value
+  const input = event.target;
+  const output = input.nextElementSibling;
+  output.value = input.value;
+  
+  // apply css-filter
+  const root = document.querySelector(':root');
+  const rootStyles = getComputedStyle(root);
+  const blurFilter = rootStyles.getPropertyValue('--blur');
+  const suffix = input.dataset.sizing;
+  root.style.setProperty(`--${input.name}`, input.value + suffix);
+ }
+
+ inputs.forEach(input => input.addEventListener('input', applyFilter));
 
 
 
-inputs.forEach(input => input.addEventListener('input', changeInputValue));
 
 
+ 
